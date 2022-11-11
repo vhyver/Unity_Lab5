6afed4e84f6b6b238da80f5df00d3dd6a2986004
Vadym Verba <71729845+vhyver@users.noreply.github.com>
2022-11-11 19:19:08 +0200

    Scene1

---
+++ Unity_Lab2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public Transform cameraPosition;
+    public float mouseSensitivity;
+    public bool invertX;
+    public bool invertY;
+
+    private CharacterController characterController;
+    private Vector3 movementVector;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        //movementVector.x = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        //movementVector.z = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+
+        Vector3 movementVertical = Input.GetAxis("Vertical") * transform.forward;
+        Vector3 movementHorizontal = Input.GetAxis("Horizontal") * transform.right;
+
+        movementVector = movementHorizontal + movementVertical;
+        movementVector.Normalize();
+        movementVector = movementVector * movementSpeed * Time.deltaTime;
+
+        characterController.Move(movementVector);
+
+        Vector2 mouseVector = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        if (invertX)
+        {
+            mouseVector.x = -mouseVector.x;
+        }
+        if (invertY)
+        {
+            mouseVector.y = -mouseVector.y;
+        }
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseVector.x, transform.rotation.eulerAngles.z);
+        cameraPosition.rotation = Quaternion.Euler(cameraPosition.rotation.eulerAngles + new Vector3(mouseVector.y * -1, 0f, 0f));
+    }
+}
